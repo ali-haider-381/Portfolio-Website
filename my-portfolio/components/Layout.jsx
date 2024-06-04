48ced7302a3535bc28e486a14db719266d0a29cb
ali-haider-381 <a1aichannel381@gmail.com>
2024-06-04 08:43:23 +0500

    Portfolio

---
+++ my-portfolio/components/Layout.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './navbar';
+import Footer from './footer';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout;
